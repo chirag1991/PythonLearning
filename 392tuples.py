08ab7f2383961a014773e86a3a0f94d5ceb13a29
Chirag Valand <chiragvaland22@gmail.com>
2022-05-09 14:42:51 -0600

    added tuples example

---
+++ 392tuples.py
@@ -0,0 +1,9 @@
+# tuples are restricted lists where valuse are set once like constant in other languages
+foods = ('Apple', 'Pizza', 'banana',)
+
+for food in foods:
+    print("Food in Foods is: ",food)
+
+
+
+
