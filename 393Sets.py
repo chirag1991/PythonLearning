fcd2500c8cbb54ff836ba2ea16d830cbdecff8ef
Chirag Valand <chiragvaland22@gmail.com>
2022-05-09 14:51:24 -0600

    added Sets example

---
+++ 393Sets.py
@@ -0,0 +1,14 @@
+# List example
+# lst = ["Item1","Item2","Item3"]
+
+# #dictionary Example
+# dictionary = {
+#     "key": "Valuse"
+# }
+
+#sets example
+
+s= {"Item1", "Item2", "Item3"}
+
+print(s)
+
